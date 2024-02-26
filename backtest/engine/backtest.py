Fix(many things): Fix a lot of bugs in stats calculation and zero divisions

backtest/engine/backtest.py
@@ -50,7 +50,6 @@ class BackTest:
                              min_initial_margin / 100, min_maintenance_margin / 100, liquidation_delay,
                              min_initial_margin_short / 100, min_maintenance_margin_short / 100)
         self.strategy = strategy
-        self.strategy.init(self.account, self.broker)
         self.main_timestep = main_timestep    # The index of the timeseries data in data list to use as the main series.
                                               # i.e. the frequency at hich the strategy is runned.
                                               # The other timeseries will be passed to the strategy as additional data.
@@ -88,16 +87,18 @@ class BackTest:
         }
         # Resolutions are added at the end of the Record objet list.
         self.time_res_extender = time_res_extender
-        self.cash_controller = cash_controller.init(self.account, self.broker)
+        self.cash_controller = cash_controller
+        self.cash_controller.init(self.account, self.broker)
         self._verbose = verbose    # 0: No print, 1: Only errors, 2: Errors and warnings, 3: All
+        self.run_iter: int = 0
 
-    def step(self, i: int, timestep: datetime):
+    def step(self, i: int, timestep: datetime, next_time_step: datetime):
         # Step 1: Prepare the data
         prepared_data: List[List[Record]] = self._prep_data(timestep)
 
         # Step 2: Filter stock data to remove the ones that are not currently available (Chart is None for main res)
         mask = self._get_mask(prepared_data[self.main_timestep])
-        prepared_data: List[npt.NDArray[Record]] = [np.array(prepared_data[i])[mask].tolist() for i in range(len(prepared_data))]
+        prepared_data: List[npt.NDArray[Record]] = [np.array(prepared_data[i])[mask] for i in range(len(prepared_data))]
 
         # Step 3: Run strategy
         # Tell the broker what datetime it is, so it can mark trade orders to this timestamp
@@ -107,7 +108,11 @@ class BackTest:
         # Step 4: Run broker
         current_data, next_tick_data, marginables, dividends, div_freq, short_rate, security_names = (
             self._prep_brokers_data(prepared_data[self.main_timestep].tolist()))
-        self.broker.tick(timestep, security_names, current_data, next_tick_data, marginables, dividends, div_freq, short_rate)
+        self.broker.tick(timestep, next_time_step, security_names, current_data, next_tick_data, marginables, dividends, div_freq,
+                         short_rate)
+
+        # Step 5: Increase run_iter
+        self.run_iter += 1
 
     @staticmethod
     def _prep_brokers_data(prepared_data: List[Record]) \
@@ -202,12 +207,12 @@ class BackTest:
         """
         start = datetime.now()
         # Step 1: Initialization
-        # Initialize strategy
-        self.strategy.init(self.account, self.broker, self.available_time_res)
-
         # Initialize the backtest by checking if the data is valid and preparing the timestep list.  It also gets the
         # columns names (features) and the tickers names.
         features, tickers, timesteps_list = self._initialize_bcktst()
+
+        # Initialize strategy
+        self.strategy.init(self.account, self.broker, self.available_time_res)
 
         # This adds freedom to the user if he has custom data
         timesteps_list = self.stadardize_timesteps(timesteps_list)
@@ -223,7 +228,7 @@ class BackTest:
 
         # Step 2: Run the backtest
         last_timestep: Optional[datetime] = None
-        for i, timestep in enumerate(tqdm(timesteps_list, desc="Backtesting...")):
+        for i, timestep in enumerate(tqdm(timesteps_list[self.window:-1], desc="Backtesting...")):
             # Run the cash controller
             if last_timestep is None:
                 last_timestep = timestep
@@ -238,15 +243,18 @@ class BackTest:
                     self.cash_controller.every_year(timestep)
                 last_timestep = timestep
 
-            self.step(i, timestep)
+            self.step(i, timestep, timesteps_list[i + self.window + 1])
 
         if self.sell_at_the_end:
             self._sell_all(timesteps_list[-1])
 
 
         # Step 3: Prepare and save stats
-        market_worth = self.market_index.data["Close"].loc[timesteps_list[0]:timesteps_list[-1]].to_numpy()
-        market_worth[0] = self.market_index.data["Open"].iloc[0]
+        if self.market_index is not None:
+            market_worth = self.market_index.data["Close"].loc[timesteps_list[0]:timesteps_list[-1]].to_numpy()
+            market_worth[0] = self.market_index.data["Open"].iloc[0]
+        else:
+            market_worth = None
         self.results = BackTestResult(self.metadata.strategy_name, metadata=self.metadata, start=timesteps_list[0],
                                       end=timesteps_list[-1], intial_cash=self._initial_cash, market_index=market_worth,
                                       broker=self.broker, account=self.account,
@@ -315,7 +323,8 @@ class BackTest:
         prepared_data = self._prepare_data(self._data, self.main_timestep, timestep, window=self.window,
                                            default_marginable=self.default_marginable,
                                            default_shortable=self.default_shortable,
-                                           default_short_rate=self.default_short_rate, save_next_tick=True)
+                                           default_short_rate=self.default_short_rate, save_next_tick=True,
+                                           next_tick_is_current=True)
         current_data, next_tick_data, marginables, dividends, div_freq, short_rate, security_names = (
             self._prep_brokers_data(prepared_data))
 
@@ -335,8 +344,8 @@ class BackTest:
     @staticmethod
     def _prepare_data(data: List[Dict[str, TSData]], current_time_res: int, timestep: datetime,
                       window: int, default_marginable: bool, default_shortable: bool, default_short_rate: float,
-                      max_look_back_dt: Optional[datetime] = None, save_next_tick: bool = False)\
-            -> List[Record]:
+                      max_look_back_dt: Optional[datetime] = None, save_next_tick: bool = False,
+                      next_tick_is_current: bool = False) -> List[Record]:
         """
         Prepare the data for the current timestep.  This method assumes that nan are padding.  This means that it is
         assumed that the security didn't exist at the time where there is nan.  It is important to fill nan in data
@@ -354,6 +363,8 @@ class BackTest:
         :param max_look_back_dt: The maximum look back timestep.  If not None, the window will start at that timestep
                                 or the nearest one before that timestep in the data's index assuming it is not padded
                                 with nan.  This can be useful for time resolutions that are not the main one.
+        :param save_next_tick: If True, it will save the current tick data as a next tick in the Record object.
+                                (This is useful for the sell all at the end of the backtest)
         :return: The prepared data
         """
         prepared_data: List[Record] = []
@@ -375,7 +386,7 @@ class BackTest:
             # The security does not exist yet
             if end_idx == start_idx:
                 prepared_data.append(Record(None, ticker, current_time_res, False, False,
-                                            ts.div_freq, 0, None))
+                                            ts.div_freq, 0))
                 continue
 
             # Security has been delisted or there is missing values.  (We will ignore it)
@@ -415,6 +426,8 @@ class BackTest:
                 cropped["Volume"] *= multiplier
                 cropped["Dividends"] *= multiplier
             if save_next_tick:
+                if next_tick_is_current:
+                    end_idx = end_idx - 1
                 next_point = ts.data[["Open", "High", "Low", "Close", "Volume", "Dividends"]].iloc[end_idx]
                 if multiplier > 0:
                     next_point["Open"] /= multiplier
@@ -482,7 +495,7 @@ class BackTest:
         :param timestep: The current timestep
         :return: A list of the forged candles: List[Tuple[Open, High, Low, Close, Volume, Stock Split, ticker]]
         """
-        if self._verbose >= 2:
+        if self._verbose >= 2 and self.run_iter == 0:
             warnings.warn("This method is not guaranteed to work for your setup.  You should override it, or make sure it "
                           "works for your setup if you have series that have a higher resolution than the main resolution.",
                           UnexpectedBehaviorRisk)

backtest/engine/backtestResult.py
@@ -13,6 +13,7 @@ import json
 from pathlib import PurePath
 from .utils import *
 import backtest
+from typing import Optional
 
 class Period(Enum):
     YEARLY = 1
@@ -25,7 +26,7 @@ class Period(Enum):
 
 class BackTestResult:
     def __init__(self, strategy_name: str, metadata: Metadata, start: datetime, end: datetime, intial_cash: float,
-                 market_index: npt.NDArray[np.float64], broker: Broker, account: Account,
+                 market_index: Optional[npt.NDArray[np.float64]], broker: Broker, account: Account,
                  risk_free_rate: float = 1.5):
         """
         This class is used to store the result of a backtest.  It contains all the information about the backtest
@@ -47,6 +48,7 @@ class BackTestResult:
         self.metadata = metadata
         self.strategy_name = strategy_name
         self.initial_cash = intial_cash
+        self.historical_states = broker.historical_states
 
         # Unique values
         self.start = start
@@ -55,17 +57,24 @@ class BackTestResult:
         self.exposure_time = broker.exposure_time
         self.equity_final = equity_history[-1]
         self.equity_peak = equity_history.max()
-        self.returns = 100 * ((equity_history[-1] - intial_cash) / intial_cash).item
+        self.returns = 100 * ((equity_history[-1] - intial_cash) / intial_cash).item()
         self.market_index = market_index
-        self.index_returns = 100 * (market_index[-1] - market_index[0]) / market_index[0]    # Buy and hold
+        if market_index is not None:
+            self.index_returns = 100 * (market_index[-1] - market_index[0]) / market_index[0]    # Buy and hold
+        else:
+            self.index_returns = None
         self.annual_returns = self._get_annual_returns(self.duration, self.returns)
-        self.sharp_ratio = (self.annual_returns - risk_free_rate) / np.std(equity_history)
+        std = np.std(equity_history)
+        if std == 0:
+            self.sharp_ratio = None
+        else:
+            self.sharp_ratio = (self.annual_returns - risk_free_rate) / np.std(equity_history)
         self.sortino_ratio = self.compute_sortino_ratio(risk_free_rate)
 
         # Now, calculate the drawdown
         drawdown = self.get_drawdown(equity_history, timestamps)
         drawdown_series = pd.Series(data=drawdown, index=pd.DatetimeIndex(timestamps))
-        yearly_drawdown = drawdown_series.resample("Y").min()
+        yearly_drawdown = drawdown_series.resample("YE", closed='left', label='left').min()
         yearly_drawdown.index = yearly_drawdown.index.year
         self.max_drawdown = drawdown.max()
         self.avg_drawdown = drawdown.mean()
@@ -124,7 +133,7 @@ class BackTestResult:
         :return: The annual returns of the strategy (esperance) in percentage
         """
         duration_in_years = duration.total_seconds() / (365*86_400)
-        return 100 * np.exp(np.log(returns) / duration_in_years)
+        return 100 * np.exp(np.log(returns / 100 + 1) / duration_in_years) - 100
 
 
     def get_ohlc(self, period: Period):
@@ -133,21 +142,23 @@ class BackTestResult:
         :param period: The period to use to make the OHLC dataframe
         :return: The OHLC dataframe
         """
-        equity_data = np.array([stepState.worth for stepState in self.broker.historical_states]).reshape(-1, 1)
-        equity_index = pd.DatetimeIndex(np.array([stepState.timestamp for stepState in self.broker.historical_states]))
+        equity_data = np.array([stepState.worth for stepState in self.historical_states]).reshape(-1, 1)
+        equity_index = pd.DatetimeIndex(np.array([stepState.timestamp for stepState in self.historical_states]))
         equity_series = pd.DataFrame(equity_data, index=equity_index)
 
         if period == Period.YEARLY:
             # Copilot generated, need to check if it works
-            return equity_series.resample("Y").ohlc()
+            out = equity_series.resample("YE", closed='left', label='left').ohlc()
         elif period == Period.QUARTERLY:
-            return equity_series.resample("Q").ohlc()
+            out = equity_series.resample("Q", closed='left', label='left').ohlc()
         elif period == Period.MONTHLY:
-            return equity_series.resample("M").ohlc()
+            out = equity_series.resample("M", closed='left', label='left').ohlc()
         elif period == Period.DAILY:
-            return equity_series.resample("D").ohlc()
+            out = equity_series.resample("D", closed='left', label='left').ohlc()
         elif period == Period.WEEKLY:
-            return equity_series.resample("W").ohlc()
+            out = equity_series.resample("W", closed='left', label='left').ohlc()
+        out.columns = out.columns.droplevel()
+        return out.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close"})
 
     def compute_sortino_ratio(self, risk_free_rate) -> float:
         """
@@ -156,44 +167,51 @@ class BackTestResult:
         :return: The sortino ratio of the strategy
         """
         equity_ohlc = self.get_ohlc(Period.YEARLY)
-        diff = equity_ohlc["close"].diff()
-        diff[0] = equity_ohlc["close"].iloc[0] - equity_ohlc["open"].iloc[0]
-        diff_percentage = 100 * diff / equity_ohlc["close"].shift(1)
+        diff = equity_ohlc["Close"].diff()
+        diff.iloc[0] = equity_ohlc["Close"].iloc[0] - equity_ohlc["Open"].iloc[0]
+        diff_percentage = 100 * diff / equity_ohlc["Close"].shift(1)
         downside = diff_percentage[diff_percentage < 0].to_numpy()
         downside_deviation = (downside ** 2).sum() / len(diff_percentage)
-        return (self.annual_returns - risk_free_rate) / downside_deviation
+        if downside_deviation == 0:
+            return None
+        else:
+            return (self.annual_returns - risk_free_rate) / downside_deviation
 
-    def compute_calmar_ratio(self, yearly_max_drawdown: pd.Series) -> float:
+    def compute_calmar_ratio(self, yearly_max_drawdown: pd.Series) -> Optional[float]:
         """
         Compute the calmar ratio of the strategy
         :param yearly_max_drawdown: The yearly maximum drawdown of the strategy
         :return: The calmar ratio of the strategy
         """
         equity_ohlc = self.get_ohlc(Period.YEARLY)
-        diff = equity_ohlc["close"].diff()
-        diff_percentage = 100 * diff / equity_ohlc["close"].shift(1)
-        diff_percentage[0] = (equity_ohlc["close"].iloc[0] - equity_ohlc["open"].iloc[0]) / equity_ohlc["open"].iloc[0]
+        diff = equity_ohlc["Close"].diff()
+        diff_percentage = 100 * diff / equity_ohlc["Close"].shift(1)
+        diff_percentage.iloc[0] = (equity_ohlc["Close"].iloc[0] - equity_ohlc["Open"].iloc[0]) / equity_ohlc["Open"].iloc[0]
         assert diff.shape == yearly_max_drawdown.shape
-        return (diff_percentage / yearly_max_drawdown).mean()
+        if (yearly_max_drawdown == 0).any():
+            return None
+        else:
+            ts_ratio = diff_percentage.to_numpy() / yearly_max_drawdown.to_numpy()
+            return ts_ratio.mean()
 
 
     def get_drawdown(self, equity_history: np.ndarray, equity_timestamps: np.ndarray,
-                     window: Union[timedelta, int] = '1y') -> npt.NDArray[np.float32]:
+                     window: Union[timedelta, int] = timedelta(days=365)) -> npt.NDArray[np.float32]:
         """
-        Get the maximum drawdown of the strategy for each timestep (Causally - so looking back inn time)
+        Get the maximum drawdown of the strategy for each timestep (Causally - so looking back in time)
         :param equity_history: The equity history of the strategy (Worth of the portoflio evolution over time)
         :param equity_timestamps: The timestamps of the equity history
         :param window: The lookback window to use to compute the drawdown
         :return: A time series of the maximum drawdown of the strategy for each time steps
         """
         if isinstance(window, timedelta):
-            df = pd.DataFrame(data=[equity_history], columns=["Worth"], index=pd.DatetimeIndex(equity_timestamps))
+            df = pd.DataFrame(data=equity_history[:, np.newaxis], columns=["Worth"], index=pd.DatetimeIndex(equity_timestamps))
             start = df.index[0]
             window_arr = df["Worth"].loc[:start + window].to_numpy()
             window = len(window_arr)
         start_maxes = np.maximum.accumulate(equity_history[:window - 1])
         maxes = np.concatenate((start_maxes, self.strided_arr(equity_history, window).max(axis=1)))
-        drawdown_continum = (window - maxes) / maxes
+        drawdown_continum = (equity_history - maxes) / maxes
         start_drawdown = np.minimum.accumulate(drawdown_continum[:window - 1])
         drawdown = np.concatenate((start_drawdown, self.strided_arr(drawdown_continum, window).min(axis=1)))
         return drawdown
@@ -297,6 +315,7 @@ class BackTestResult:
         self.min_trade_duration = data["stats"]["min_trade_duration"]
         self.profit_factor = data["stats"]["profit_factor"]
         self.sqn = data["stats"]["sqn"]
+        self.historical_states = broker.historical_states
         return self
 
     def save(self, path: str):

backtest/engine/portfolio.py
@@ -629,18 +629,26 @@ class Portfolio:
                               dtype=np.float32)
         duration_seconds = [trade.duration.total_seconds() for trade in self._trades if isinstance(trade, TradeStats)]
         # df = pd.DataFrame({"Relative Profit": rel_profit, "Absolute Profit": abs_profit, "Duration": duration})
-        best_trade = max(rel_profit)
-        worst_trade = min(rel_profit)
-        win_rate = 100 * len([x for x in rel_profit if x > 0]) / len(rel_profit)
-        avg_trade = sum(rel_profit) / len(rel_profit)
-        max_trade_duration = max(duration_seconds) / 86_400  # In days
-        min_trade_duration = min(duration_seconds) / 86_400  # In days
-        avg_trade_duration = (sum(duration_seconds) / len(duration_seconds)) / 86_400  # In days
+        best_trade = max(rel_profit) if len(rel_profit) > 0 else None
+        worst_trade = min(rel_profit) if len(rel_profit) > 0 else None
+        win_rate = 100 * len([x for x in rel_profit if x > 0]) / len(rel_profit) if len(rel_profit) > 0 else None
+        avg_trade = sum(rel_profit) / len(rel_profit) if len(rel_profit) > 0 else None
+        if len(duration_seconds) == 0:
+            max_trade_duration = None
+            min_trade_duration = None
+            avg_trade_duration = None
+        else:
+            max_trade_duration = max(duration_seconds) / 86_400  # In days
+            min_trade_duration = min(duration_seconds) / 86_400  # In days
+            avg_trade_duration = (sum(duration_seconds) / len(duration_seconds)) / 86_400  # In days
         total_gains = abs_profit[abs_profit >= 0].sum()
         total_losses = abs_profit[abs_profit < 0].sum()
         rel_profit = np.array(rel_profit, dtype=np.float32)
-        sqn = np.sqrt(self.get_trade_count(exit_only=True)) * (rel_profit.mean() / 100) / (
-                    (rel_profit / 100).std() or np.nan)
+        if len(rel_profit) == 0:
+            sqn = None
+        else:
+            sqn = np.sqrt(self.get_trade_count(exit_only=True)) * (rel_profit.mean() / 100) / (
+                        (rel_profit / 100).std())
         if total_losses == 0:
             profit_factor = total_gains
         else: