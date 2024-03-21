aa20b22d1ab3dc19f9cffcc4fc1b1c8035012f01
Anthony Lavertu <alavertu2@gmail.com>
2024-03-21 16:52:34 -0400

    Feat(EmailReportRenderer)

---
+++ src/backtest/serve/renderers/email_renderer.py
@@ -0,0 +1,77 @@
+from pathlib import PurePath
+from .markup_renderer import MarkupObject, MarkupRenderer, format_number
+from ..state_signals import StateSignals, ServerStatus
+from typing import Optional
+import os
+import shutil
+try:
+    import plotly.graph_objects as go
+    PLOTLY_INSTALLED = True
+except ImportError:
+    PLOTLY_INSTALLED = False
+
+
+TEMPLATE_PATH = PurePath(f"{os.path.dirname(__file__)}/html_templates")
+
+def format_lim(a: Optional[float]) -> str:
+    """
+    Replace a with N/A if a is None.  Else, it returns the float with a $ sign.
+    :param a: Float
+    :return: str
+    """
+    if a is None:
+        return "N/A"
+    return f"{a:,}".replace(",", " ") + "$"
+
+class EmailRenderer(MarkupRenderer):
+    """
+    Render the trading signals to a report that can be read from a mobile device or desktop.
+    """
+    def __init__(self, style: str = "light"):
+        super().__init__()
+        styles = ["light", "dark", "rich"]
+        if style not in styles:
+            raise ValueError(f"Style {style} does not exists.  Available styles are {styles}")
+        self.style = style
+
+    def render(self, state: StateSignals, base_path: PurePath):
+
+        card_template = self.components["SIGNAL_CARD"]
+        cards = []
+        for signal in state:
+            if signal.timestamp == state.timestamp:
+                # New signal
+                cards.append(card_template.render(dict(new='<span class="signal-badge">New</span>', dt=signal.timestamp,
+                                                       ticker=signal.security, amount=signal.amount,
+                                                       amount_borrowed=signal.amount_borrowed,
+                                                       price_l=format_lim(signal.security_price_limit[0]),
+                                                       price_h=format_lim(signal.security_price_limit[1]),
+                                                       expiry=signal.expiry)))
+            else:
+                cards.append(card_template.render(dict(new='', dt=signal.timestamp, ticker=signal.security,
+                                                       amount=signal.amount, amount_borrowed=signal.amount_borrowed,
+                                                       price_l=format_lim(signal.security_price_limit[0]),
+                                                       price_h=format_lim(signal.security_price_limit[1]),
+                                                       expiry=signal.expiry)))
+        self.prerender("SCROLL_SIGNALS", cards="\n".join(cards))
+
+        if state.status == ServerStatus.ERROR:
+            color = "red"
+        elif state.status == ServerStatus.WARNING:
+            color = "orange"
+        else:
+            color = "green"
+
+        # Handle css
+        css = self.get_style(self.style)
+
+        # Render the whole
+        html_content = self.render_template(status_color=color, status=state.status.name, title="Financial Report",
+                                            style=css, script="")
+
+        # Save the file
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
+
+        with open(base_path / "index.html", "w") as f:
+            f.write(html_content)
